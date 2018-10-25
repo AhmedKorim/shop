fe85241afe3f40a312411a60d7f4b9f3ec3fb5c8
ahmedkorim <ahmedkorrim@gmail.com>
2018-10-25 12:17:52 +0200

    auth actions

--- client/src/Componenents/UI/UserWidget/UserWidget.js
+++ client/src/Componenents/UI/UserWidget/UserWidget.js
@@ -8,13 +8,18 @@ import ListItem from "@material-ui/core/ListItem/ListItem";
 import Tooltip from "@material-ui/core/Tooltip/Tooltip";
 import Typography from "@material-ui/core/Typography/Typography";
 import React from 'react'
+import {connect} from 'react-redux';
 import {withRouter} from "react-router-dom";
+import {logout} from "../../../Store/userAction";
 import './UserWidget.scss';
 
 const UserWidget = props => {
-    const {} = props;
+    const {
+        user,
+        logout
+    } = props;
 
-    const HIText = true ? 'login now' : false === 'female' ? 'welcome MRS ' : 'welcome MR ' + "name";
+    const HIText = user ? `Welcome ${user.name}` : 'Login now'
     return (
         <div className="userWidget">
             <Grid container justify="center">
@@ -22,10 +27,11 @@ const UserWidget = props => {
                     <header>
                         <Grid container justify="center" alignItems="center" className="noWrapper">
                             <Grid item xs={3}>
-                                <Avatar>{"avatar".toUpperCase()}</Avatar>
+                                <Avatar className="UserWidgetAvatar"
+                                >{user ? (user.name).trim().slice(0, 2).toUpperCase() : "AN"}</Avatar>
                             </Grid>
                             <Grid item container alignItems="center" justify="center" xs={7} className="userMetaData">
-                                <Grid item xs={12}><Typography className="typo1">{"name"}</Typography></Grid>
+                                <Grid item xs={12}><Typography className="typo1">{user ? user.name : "Anonymous"}</Typography></Grid>
                                 <Grid item xs><Tooltip title={HIText}><Typography className="typo2">{HIText}</Typography></Tooltip></Grid>
                             </Grid>
                             <Grid item xs>
@@ -38,7 +44,7 @@ const UserWidget = props => {
                 <Grid item xs>
                     <div>
                         <List component="ul" className="userListItem">
-                            <ListItem component="li" button className="userListItem" onClick={void 0}>
+                            <ListItem component="li" button className="userListItem" onClick={() => props.history.push('/admin_area')}>
                                 <Grid container justify="center" alignItems="center">
                                     <Grid item xs={4}>
                                         <div><Icon className="iconCol">security</Icon></div>
@@ -46,7 +52,7 @@ const UserWidget = props => {
                                     <Grid item xs><Typography className="typo2">Admin Area</Typography></Grid>
                                 </Grid>
                             </ListItem>
-                            {true ? <ListItem component="li" button className="userListItem" onClick={() => props.history.push('/auth')}>
+                            {!user ? <ListItem component="li" button className="userListItem" onClick={() => props.history.push('/auth/login')}>
                                     <Grid container justify="center" alignItems="center">
                                         <Grid item xs={4}>
                                             <div><Icon className="iconCol">lock</Icon></div>
@@ -56,7 +62,7 @@ const UserWidget = props => {
                                     </Grid>
                                     <Divider/>
                                 </ListItem> :
-                                <ListItem component="li" button className="userListItem" onClick={void 0}>
+                                <ListItem component="li" button className="userListItem" onClick={logout}>
                                     <Grid container justify="center" alignItems="center">
                                         <Grid item xs={4}>
                                             <div><Icon className="iconCol">power_settings_new</Icon></div>
@@ -71,16 +77,11 @@ const UserWidget = props => {
         </div>
     )
 };
-/*const mapStateToProps = state => {
-    return {
-        userIfo: state.user.info
-    }
-};
+const mapStateToProps = state => ({
+    user: state.auth.user
+})
 
-const mapDispatchToProps = dispatch => {
-    return {
-        logout: () => dispatch(logout())
-    }
-};*/
-// export default /*connect(mapStateToProps, mapDispatchToProps)(withRouter(*/UserWidget/*))*/;
-export default withRouter(UserWidget);
+const mapDispatchToProps = dispatch => ({
+    logout: _ => dispatch(logout())
+})
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserWidget));
